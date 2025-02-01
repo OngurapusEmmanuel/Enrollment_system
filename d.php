89d41ad6b8c7cecd4cfe3674e7327ebdab691e8a
OngurapusEmmanuel <emmanuelonguraous@gmail.com>
2025-02-01 21:32:54 +0300

    update

--- d.php
+++ d.php
@@ -1,3 +1,72 @@
+<?php
+
+
+require('includes/config.php');
+// require('includes/sessions.php');
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+//   echo "Form submitted.<br>";
+    $email = $_POST["email"];
+    $password = $_POST["password"];
+    // echo "Email: $email<br>";
+
+    if ($con) {
+        $sql = "SELECT `Firstname`, `Password`, `Role`, `First_login` FROM users WHERE `Email` = ? ";
+// echo "SQL Query: " . $sql;
+
+$stmt = $con->prepare($sql);
+if (!$stmt) {
+    die("SQL Error: " . $con->error);
+}
+        $stmt->bind_param('s', $email);
+        $stmt->execute();
+        $stmt->store_result();
+         $stmt->bind_result($Firstname, $Password, $Role, $First_login);
+        $stmt->fetch();
+
+        if ($stmt->num_rows === 0) {
+            $_SESSION['error'] = "Email not found!";
+            header("Location: login.php");
+            exit();
+        }
+
+
+        if (!password_verify($password, $Password)) {
+            $_SESSION['error'] = "Invalid password!";
+            header("Location: login.php");
+            exit();
+        }else{
+
+        $_SESSION["name"] = $Firstname;
+        $_SESSION["role"] = $Role;
+        $_SESSION["email"] = $email;
+
+        if ($Role === "admin" || $Role === "moderator") {
+            header("Location: admin-dashboard.php");
+            exit();
+        } elseif ($Role === "user") {
+            $_SESSION["first_login"] = $First_login;
+
+            if ($First_login == 1) {
+                header("Location: update_password.php");
+                exit();
+            } else {
+                header("Location: user-dashboard.php");
+                exit();
+            }
+        } else {
+            $_SESSION['error'] = "Invalid role!";
+            header("Location: login.php");
+            exit();
+        }
+        }
+    } else {
+        die("Database connection failed: " . $con->connect_error);
+    }
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -5,7 +74,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Client-Login</title>
+    <title>Login</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet"
      integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -14,70 +83,10 @@
 </head>
 
 <body>
-<?php
-include_once('includes/sessions.php');
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = $_POST["email"];
-    $password = $_POST["password"];
-    include_once "includes/config.php";
-
-    if ($con) {
-        $stmt = $con->prepare("SELECT Firstname, `Password`, `Role`, `First_login` FROM users WHERE Email = ?");
-        $stmt->bind_param('s', $email);
-        $stmt->execute();
-        $stmt->store_result();
-        if ($stmt->num_rows === 0) {
-            $_SESSION['error'] = "Email not found!";
-            header("Location: login.php");
-            exit();
-        }
-
-        $stmt->bind_result($Firstname, $Password, $Role, $First_login);
-        $stmt->fetch();
-
-        // if (!password_verify($password, $Password))
-        if (!$password===$Password)
-         {
-            $_SESSION['error'] = "Invalid password!";
-            header("Location: login.php");
-            exit();
-        }
-
-        if ($Role === "admin" || $Role === "moderator") {
-            $_SESSION["name"] = $Firstname;
-            $_SESSION["role"] = $Role;
-            header("Location: admin-dashboard.php");
-            exit();
-        } elseif ($Role === "user") {
-            $_SESSION["name"] = $Firstname;
-            $_SESSION["role"] = $Role;
-            $_SESSION["first_login"] = $First_login;
-            $_SESSION["email"]=$email;
-            
-            // Check if it's the first login
-            if ($First_login == 1) {
-                header("Location: update_password.php");  // Redirect to password update page
-                exit();
-            } else {
-                header("Location: user-dashboard.php");  // Regular user dashboard
-                exit();
-            }
-        } else {
-            $_SESSION['error'] = "Invalid role!";
-            header("Location: login.php");
-            exit();
-        }
-    } else {
-        die("Database connection failed: " . mysqli_connect_error());
-    }
-}
-?>
-
-
 
     <!-- client login form -->
     <div class="container">
-        <form action="" method="post">
+        <form  method="post">
            
             <div class="imgcontainer">
                 <img src="images/user.png" alt="Avatar" class="avatar">
@@ -85,10 +94,10 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
             <div class="container">
                 <label for="email"><b>Email</b></label>
-                <input type="email" placeholder="Enter Email" name="email" required>
+                <input type="email" placeholder="Enter Email" id="email" name="email" required>
 
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="password" id="pass" required>
+                <label for="password"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="password" id="password" required>
 
                 <button type="submit" name="submit">Login</button>
                 <label>

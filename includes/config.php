5570c6f9a1a71dc63aa8750c29d0365fbb156663
OngurapusEmmanuel <emmanuelonguraous@gmail.com>
2025-03-23 10:03:28 +0300

    update

--- includes/config.php
+++ includes/config.php
@@ -1,14 +1,30 @@
 <?php
+$servername = " localhost:3306"; // Or your hosted server address
+$username = "bethelme_Emmanueltech"; // Your database username
+$password = "Emmerhnuel@4"; // Your database password
+$dbname = "bethelme_Bethel"; // Your database name
 
-$host = 'localhost';
-$user = 'root';
-$pwd = '';
-$db = 'enrollment';
+// Create connection
+$con = new mysqli($servername, $username, $password, $dbname);
 
-$con = new mysqli($host, $user, $pwd, $db);
-
-if ($con->connect_errno) {
-    die("Connection failed: " . $con->connect_error);
+// Check connection
+if ($con->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
+// echo "Connected successfully";
+?>
+
+<?php
+
+// $host = 'localhost';
+// $user = 'root';
+// $pwd = '';
+// $db = 'enrollment';
+
+// $con = new mysqli($host, $user, $pwd, $db);
+
+// if ($con->connect_errno) {
+//     die("Connection failed: " . $con->connect_error);
+// }
 
 ?>
